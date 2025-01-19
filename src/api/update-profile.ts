371dbe73fd2c66cee0e1364251ce341cddeb3aeb
Gustavo <gbarska@yahoo.com>
2025-01-18 22:18:01 -0300

    Adding  profile and restaurant queries

---
+++ src/api/update-profile.ts
@@ -0,0 +1,8 @@
+import { api } from '@/lib/axios'
+interface UpdateProfileBody {
+  name: string
+  description: string | null
+}
+export async function updateProfile({ description, name }: UpdateProfileBody) {
+  await api.put('/profile', { name, description })
+}
