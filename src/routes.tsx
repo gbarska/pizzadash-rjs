245020787b07d4cf554e1a8aff5cc5d181329151
Gustavo <gbarska@yahoo.com>
2025-01-26 19:27:39 -0300

    Adding loading and handling errors page

--- src/routes.tsx
+++ src/routes.tsx
@@ -7,6 +7,7 @@ import { Dashboard } from './pages/app/dashboard/dashboard'
 import { Orders } from './pages/app/orders/orders'
 import { SignIn } from './pages/auth/sign-in'
 import { SignUp } from './pages/auth/sign-up'
+import { Error } from './pages/error'
 
 export const router = createBrowserRouter([
   // without layouts
@@ -17,7 +18,7 @@ export const router = createBrowserRouter([
   {
     path: '/',
     element: <AppLayout />,
-    errorElement: <NotFound />,
+    errorElement: <Error />,
     children: [
       { path: '/', element: <Dashboard /> },
       {
@@ -34,5 +35,9 @@ export const router = createBrowserRouter([
   {
     path: '/sign-up',
     element: <SignUp />,
+  },
+  {
+    path: '*',
+    element: <NotFound />,
   },
 ])
